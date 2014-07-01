7538ae17ef8c3658e0d4aa85772b9340b002f734
Feifan Zhou <Feifan@Feifans-MacBook-Pro.local>
2014-06-30 21:39:49 -0400

    Create new tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,23 @@
 class TagsController < ApplicationController
+  include ApplicationHelper
+  def index
+    if is_not_contributor
+      render status: :unauthorized, html: 'If you are a contributor, please <a href="/contributors/login">login</a>'.html_safe
+    end
+    @tags = Tag.all
+  end
+
+  def new
+    redirect_to tags_path
+  end
+
+  def create
+    t = Tag.new(tag_params)
+    t.contributor = active_contributor
+    t.save
+    redirect_to tags_path
+  end
+
   def show
     @tag = Tag.find(params[:id])
     @posts = @tag.posts_for_page(1, 10).select { |post| !post.blank? }
@@ -14,5 +33,10 @@ class TagsController < ApplicationController
       # format.html
       format.naked { render partial: 'shared/tag_suggestions', formats: [:html], locals: { tags: @tags } }
     end
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name, :tag_type)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
   def is_contributor
     !cookies.signed[:remember_token].blank? && Contributor.exists?(remember_token: cookies.signed[:remember_token])
   end
+
+  def active_contributor
+    return nil if is_not_contributor
+    Contributor.find_by_remember_token(cookies.signed[:remember_token])
+  end
 end
