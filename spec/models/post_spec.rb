7942ddbd5757e4b194a79e9af2077e00a312075a
Feifan Zhou <fz75@cornell.edu>
2015-01-22 09:09:07 -0500

    Change all orange to Tunetap orange
    Ref T130

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -105,7 +105,7 @@ describe 'Post embed code' do
     post = FactoryGirl.create :post
     post.process_player_embed('<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/151835201&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>')
     expect(post.player_type).to eq('soundcloud')
-    expect(post.player_embed).to eq('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/151835201&amp;color=F6921E&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;')
+    expect(post.player_embed).to eq('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/151835201&amp;color=F16214&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;')
   end
 end
 
@@ -121,7 +121,7 @@ describe 'Post embed code' do
   it 'generates correctly for Soundcloud embed' do
     post.process_player_embed '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43547217&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
     expect(post.player_type).to eq 'soundcloud'
-    expect(post.embed_code).to eq "<iframe width='100%' height='180' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43547217&amp;color=F6921E&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;'></iframe>"
+    expect(post.embed_code).to eq "<iframe width='100%' height='180' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43547217&amp;color=F16214&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;'></iframe>"
   end
 
   it 'generates correctly for Spotify URI' do
@@ -159,17 +159,17 @@ describe 'Post embed code' do
   it 'generates correctly for Vimeo embed' do
     post.process_player_embed '<iframe src="//player.vimeo.com/video/97897747?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ffffff" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     expect(post.player_type).to eq 'vimeo'
-    expect(post.embed_code).to eq "<iframe src='//player.vimeo.com/video/97897747?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=F6921E' width='100%' height='100%' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    expect(post.embed_code).to eq "<iframe src='//player.vimeo.com/video/97897747?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=F16214' width='100%' height='100%' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
   end
   it 'generates correctly for Vimeo detailed URL' do
     post.process_player_embed 'http://vimeo.com/channels/darkrye/91720717'
     expect(post.player_type).to eq 'vimeo'
-    expect(post.embed_code).to eq "<iframe src='//player.vimeo.com/video/91720717?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=F6921E' width='100%' height='100%' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    expect(post.embed_code).to eq "<iframe src='//player.vimeo.com/video/91720717?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=F16214' width='100%' height='100%' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
   end
   it 'generates correctly for Vimeo short URL' do
     post.process_player_embed 'http://vimeo.com/17111870'
     expect(post.player_type).to eq 'vimeo'
-    expect(post.embed_code).to eq "<iframe src='//player.vimeo.com/video/17111870?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=F6921E' width='100%' height='100%' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    expect(post.embed_code).to eq "<iframe src='//player.vimeo.com/video/17111870?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=F16214' width='100%' height='100%' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
   end
 
 end
